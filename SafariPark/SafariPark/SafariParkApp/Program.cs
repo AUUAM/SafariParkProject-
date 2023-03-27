e45a4830965a0af2bcc1953b03dca4b2a2fcd327
Kevin Conceicao <kevinpc1997@hotmail.co.uk>
2023-03-27 14:37:43 +0100

    final

--- SafariPark/SafariPark/SafariParkApp/Program.cs
+++ SafariPark/SafariPark/SafariParkApp/Program.cs
@@ -15,7 +15,7 @@ namespace SafariParkApp
             Hunter tahsin = new Hunter("Tahsin", "CH", new Camera("Nikon"));
             Hunter kevin = new Hunter("Kevin", "CO", new Camera("Canon"));
 
-
+/*
 
             list.Add(lazer);
             list.Add(water);
@@ -29,44 +29,44 @@ namespace SafariParkApp
             }
 
             kevin.Shooter = lazer;
-            Console.WriteLine(kevin.Shoot());
+            Console.WriteLine(kevin.Shoot());*/
 
 
 
             // Extra stuff
 
-            /*            Console.WriteLine("Enter your name: ");
-                        string name = Console.ReadLine(); //entering name
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine(); //entering name
 
-                        Console.WriteLine("Choose your weapon: ");
-                        Console.Write("1. Water Pistol \n2. Lazer Gun");
-                        Console.WriteLine();
+            Console.WriteLine("Choose your weapon: ");
+            Console.Write("1. Water Pistol \n2. Lazer Gun");
+            Console.WriteLine();
 
-                        string option = Console.ReadLine();
-                        //int opt = Convert.ToInt32(option); //choosing weapon
+            string option = Console.ReadLine();
+            //int opt = Convert.ToInt32(option); //choosing weapon
 
-                        if (option == "1")
-                        {
-                            Hunter inputName = new Hunter(name,"", new Camera("Nikon"));
-                            inputName.Shooter = water;
-                            list.Add(inputName);
-                            foreach (var weapon in list)
-                            {
-                                Console.WriteLine(weapon.Shoot());
+            if (option == "1")
+            {
+                Hunter inputName = new Hunter(name, "", new Camera("Nikon"));
+                inputName.Shooter = water;
+                list.Add(inputName);
+                foreach (var weapon in list)
+                {
+                    Console.WriteLine(weapon.Shoot());
 
-                            }
-                        }
-                        else if (option == "2")
-                        {
-                            Hunter inputName = new Hunter(name,"", new Camera("Nikon"));
-                            inputName.Shooter = lazer;
-                            list.Add(inputName);
-                            foreach (var weapon in list)
-                            {
-                                Console.WriteLine(weapon.Shoot());
+                }
+            }
+            else if (option == "2")
+            {
+                Hunter inputName = new Hunter(name, "", new Camera("Nikon"));
+                inputName.Shooter = lazer;
+                list.Add(inputName);
+                foreach (var weapon in list)
+                {
+                    Console.WriteLine(weapon.Shoot());
 
-                            }
-                        }*/
+                }
+            }
 
         }
     }
